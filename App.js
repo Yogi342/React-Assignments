fa6fdfae0f7ed12fbb9299418b7bda6c389611c6
Yogesh Dodeja <109654902+Yogi342@users.noreply.github.com>
2024-04-29 19:44:44 +0530

    Done

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import Categories from "./Components/Categories";
+import Header from "./Components/Header";
+import Slider from "./Components/Slider";
+import "./css.scss";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Slider />
+      <Categories />
+    </div>
+  );
+}
+
+export default App;
